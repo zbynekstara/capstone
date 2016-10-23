bde717b612ee384864c96e25838ae5eb6ec0b3e0
zs633 <zs633@nyu.edu>
2016-10-23 15:51:01 +0400

    editing 0_all script

--- Capstone Scripts/0_all.bat
+++ Capstone Scripts/0_all.bat
@@ -7,35 +7,49 @@ REM "Requires matplotlib (available through Anaconda)"
 
 SET scripts_path="C:\Users\zs633\Capstone\Capstone Scripts"
 
-ECHO "Step 1: Random valid trips"
+ECHO Executing capstone scripts
+
+ECHO Step 1: Random valid trips
+ECHO %DATE% %TIME%
 CALL %scripts_path%\1_random_valid_trips
 
-ECHO "Step 2: Duarouter routes"
+ECHO Step 2: Duarouter routes
+ECHO %DATE% %TIME%
 CALL %scripts_path%\2_duarouter_routes
 
-ECHO "Step 3: Edge dump"
+ECHO Step 3: Edge dump
+ECHO %DATE% %TIME%
 CALL %scripts_path%\3_edge_dump
 
-ECHO "Step 4: Convert to plain format"
+ECHO Step 4: Convert to plain format
+ECHO %DATE% %TIME%
 CALL %scripts_path%\4_convert_to_plain_format
 
-ECHO "Step 5: Apply weights"
+ECHO Step 5: Apply weights
+ECHO %DATE% %TIME%
 CALL %scripts_path%\5_apply_weights
 
-ECHO "Step 6: Convert to gv format"
+ECHO Step 6: Convert to gv format
+ECHO %DATE% %TIME%
 CALL %scripts_path%\6_convert_to_gv_format
 
-ECHO "Step 7: Apply Neato"
+ECHO Step 7: Apply Neato
+ECHO %DATE% %TIME%
 CALL %scripts_path%\7_apply_neato
 
-ECHO "Step 8: Convert to plain format"
+ECHO Step 8: Convert to plain format
+ECHO %DATE% %TIME%
 CALL %scripts_path%\8_convert_to_plain_format
 
-ECHO "Step 9: Convert to net format"
+ECHO Step 9: Convert to net format
+ECHO %DATE% %TIME%
 CALL %scripts_path%\9_convert_to_net_format
 
-ECHO "Step 10: Color traffic maps"
+ECHO Step 10: Color traffic maps
+ECHO %DATE% %TIME%
 CALL %scripts_path%\10_color_traffic_maps
 
 ECHO All done!
+ECHO %DATE% %TIME%
+PAUSE
 EXIT 0
