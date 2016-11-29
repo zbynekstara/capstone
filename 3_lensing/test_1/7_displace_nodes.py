9d0e0548d6ff0315b8334408fa69b28962a54de7
zs633 <zs633@nyu.edu>
2016-11-29 13:55:03 +0400

    lensing test 1

---
+++ 3_lensing/test_1/7_displace_nodes.py
@@ -0,0 +1,229 @@
+# -*- coding: utf-8 -*-
+
+import math
+import xml.etree.ElementTree as ET
+
+# OPEN FILES
+print "open files"
+
+node_file = open('./6_prepared/modified.nod.xml', 'r')
+edge_file = open('./6_prepared/modified.edg.xml', 'r')
+
+lensing_node_file = open('./7_displaced/lensing.nod.xml', 'w')
+lensing_edge_file = open('./7_displaced/lensing.edg.xml', 'w')
+info_file = open('./10d_info/stats.google.txt', 'w')
+
+# READ NODE FILE TO GET NODES
+print "read node file"
+node_tree = ET.parse(node_file)
+node_root = node_tree.getroot()
+
+file_nodes = node_root.iter('node')
+node_subtree = ET.Element('node_subtree_root')
+node_len = 0
+for file_node in file_nodes:
+	node_subtree.append(file_node)
+	node_len += 1
+print str(node_len)
+
+# READ EDGE FILE TO GET EDGES
+print "read edge file"
+edge_tree = ET.parse(edge_file)
+edge_root = edge_tree.getroot()
+
+file_edges = edge_root.iter('edge')
+edge_subtree = ET.Element('edge_subtree_root')
+edge_len = 0
+for file_edge in file_edges:
+	edge_subtree.append(file_edge)
+	edge_len += 1
+print str(edge_len)
+
+# CONSTRCT TRANSLATION DICTIONARY
+print "construct translation dictionary"
+lensing_translation_dict = dict()
+c = 0.0 # q/r
+
+# first part, calculating a based on c while lensing is possible
+current_a = 0.0000
+sum_c = 0.00000
+num_c = 0.0
+while ((c/math.sqrt(1.0-math.pow(c,2)))+c) < 1.00000:
+	a = c/math.sqrt(1.0-math.pow(c,2))
+
+	if round(a,4) != current_a: # reset for new numbers
+		current_a = round(a,4)
+		sum_c = 0.00000
+		num_c = 0
+
+	sum_c += c
+	num_c += 1.0
+
+	#print "a="+str(current_a)+" c="+str(sum_c/num_c)
+	lensing_translation_dict[str(current_a)] = round(sum_c/num_c,4)
+	
+	#b = a + c # y/r
+	c += 0.00001
+
+# second part, continuing so that b would be 1 for everything
+# this is because of the total internal reflection phenomenon
+current_a += 0.0001
+while current_a <= 1.0000:
+	c = round((1.0000-current_a),4)
+
+	#print "a="+str(current_a)+" c="+str(c)
+	lensing_translation_dict[str(current_a)] = c
+
+	current_a += 0.0001
+
+#print lensing_translation_dict
+
+# DESIGNATE FIXED NODES
+print "designate fixed nodes"
+edges = edge_subtree.iter('edge')
+for edge in edges:
+	edge_speed = float(edge.get('speed'))
+	if edge_speed > 15:
+		# 15 mps = 50 kph/30 mph
+		# 18 mps = 60 kph/40 mph
+		edge_from = edge.get('from')
+		edge_to = edge.get('to')
+
+		node_from = node_subtree.find('node[@id=\''+edge_from+'\']')
+		node_from.set('fixed','True')
+
+		node_to = node_subtree.find('node[@id=\''+edge_to+'\']')
+		node_to.set('fixed','True')
+
+# DESIGNATE JUNCTIONS
+print "designate junctions"
+nodes = node_subtree.iter('node')
+#num_junctions = 0
+for node in nodes:
+	node_weight_num = float(node.get('weight_num',0.0))
+	if node_weight_num > 1.0:
+		node.set('junction','True')
+		#num_junctions += 1
+
+"""#fixed_cutoff = int(0.25 * float(num_junctions)) # CONSTANT
+nodes = node_subtree.iter('node')
+#counter = 0
+for node in nodes:
+	#if counter > fixed_cutoff:
+	#	break
+
+	node_speed = float(node.get('speed',-1.0))
+	#is_junction = bool(node.get('junction',False))
+	#if is_junction:
+	if node_speed > 15: # just above 50 kph - only major roads
+		node.set('fixed','True')
+
+	#counter += 1"""
+
+# DISPLACE NODES BASED ON THE LENSING EFFECT OF MOST CONGESTED NODES
+print "displace nodes"
+max_radius = 900.0 # CONSTANT # max raindrop radius in meters - for 100% congested junction
+
+nodes = node_subtree.iter('node')
+counter = 0
+for node in nodes:
+	print str(counter)
+	is_junction = bool(node.get('junction',False))
+	#is_fixed = bool(node.get('fixed',False))
+
+	#if is_junction and is_fixed: # if this node can move things
+	if is_junction: # if this node can move things
+		node_weight = float(node.get('avg_slowdown_ratio', 0.0))
+		radius = max_radius * node_weight # raindrop radius
+
+		this_x = float(node.get('x'))
+		this_y = float(node.get('y'))
+
+		other_nodes = node_subtree.iter('node')
+		for other_node in other_nodes:
+			is_fixed = bool(other_node.get('fixed',False))
+			if not is_fixed: # if this node can be moved
+				other_x = float(other_node.get('x'))
+				other_y = float(other_node.get('y'))
+
+				nodes_distance = math.hypot(abs(other_x-this_x),abs(other_y-this_y)) # x
+				if nodes_distance > 0.0 and nodes_distance < radius:
+					a = round(nodes_distance/radius,4)
+					c = lensing_translation_dict[str(a)]
+
+					multiplier = c/a
+					other_x_displacement = multiplier * (other_x-this_x)
+					other_y_displacement = multiplier * (other_y-this_y)
+
+					other_x_displacement_sum = float(other_node.get('x_displacement_sum',0.0))
+					other_y_displacement_sum = float(other_node.get('y_displacement_sum',0.0))
+
+					other_node.set('x_displacement_sum',str(other_x_displacement_sum+other_x_displacement))
+					other_node.set('y_displacement_sum',str(other_y_displacement_sum+other_y_displacement))
+
+	counter += 1
+
+nodes = node_subtree.iter('node')
+for node in nodes:
+	old_x = float(node.get('x'))
+	old_y = float(node.get('y'))
+
+	node.set('old_x',str(old_x))
+	node.set('old_y',str(old_y))
+
+	x_displacement_sum = float(node.get('x_displacement_sum',0.0))
+	y_displacement_sum = float(node.get('y_displacement_sum',0.0))
+
+	new_x = old_x + x_displacement_sum
+	new_y = old_y + y_displacement_sum
+
+	node.set('x',str(new_x))
+	node.set('y',str(new_y))
+
+# REMOVE EDGE SHAPES
+print "remove edge shapes"
+edges = edge_subtree.iter('edge')
+for edge in edges:
+	edge.attrib.pop('shape',None) # get rid of the shape attribute
+
+# PREPARE INFO FILE FOR GOOGLE COLORS
+print "prepare info file"
+edges = edge_root.iter('edge')
+# google colormap in terms of slowdown_ratio: 0.00-0.23, 0.23-0.54, 0.54-0.92, 0.92-1.00
+black_edges = 0
+red_edges = 0
+yellow_edges = 0
+green_edges = 0
+for edge in edges:
+	slowdown_ratio = float(edge.get('slowdown_ratio',0.0))
+	if slowdown_ratio > 0.92:
+		black_edges += 1
+		continue
+	if slowdown_ratio > 0.54:
+		red_edges += 1
+		continue
+	if slowdown_ratio > 0.23:
+		yellow_edges += 1
+		continue
+	# else
+	green_edges += 1
+
+info_string = ""
+info_string += "black edges: "+str(black_edges)+"\n"
+info_string += "red edges: "+str(red_edges)+"\n"
+info_string += "yellow edges: "+str(yellow_edges)+"\n"
+info_string += "green edges: "+str(green_edges)+"\n"
+
+# WRITE OUTPUT FILES
+print "write output files"
+node_tree.write(lensing_node_file,encoding='UTF-8',xml_declaration=True)
+edge_tree.write(lensing_edge_file,encoding='UTF-8',xml_declaration=True)
+info_file.write(info_string)
+
+# CLOSE FILES
+print "close files"
+node_file.close()
+edge_file.close()
+lensing_node_file.close()
+lensing_edge_file.close()
+info_file.close()
